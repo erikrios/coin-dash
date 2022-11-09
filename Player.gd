f6d37c696abf925be05a50c5514182de061340d7
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-11-09 17:59:41 +0700

    Create Player scene and script

---
+++ Player.gd
@@ -0,0 +1,53 @@
+extends Area2D
+
+signal pickup
+signal hurt
+
+export (int) var speed
+var velocity = Vector2()
+var screen_size = Vector2(480, 720)
+
+func _process(delta):
+	get_input()
+	
+	position += velocity * delta
+	
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite.animation = "run"
+		$AnimatedSprite.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite.animation = "idle"
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+func start(pos):
+	set_process(true)
+	position = pos
+	$AnimatedSprite.animation = "idle"
+	
+func die():
+	$AnimatedSprite.animation = "hurt"
+	set_process(false)
+
+
+func _on_Player_area_entered(area):
+	if area.is_in_group("coins"):
+		area.pickup()
+		emit_signal("pickup")
+	if area.is_in_group("obstacles"):
+		emit_signal("hurt")
+		die()
