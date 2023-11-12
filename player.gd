3954e577304d7b18b34b401e1d6559aec3ccadf1
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-12 20:01:35 +0700

    Moving the player

--- player.gd
+++ player.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
-@export var speed = 350
-var velocity = Vector2.ZERO
-var screensize = Vector2(480, 720)
+@export var speed := 350
+var velocity := Vector2.ZERO
+var screensize := Vector2(480, 720)
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(delta: float) -> void:
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += velocity * speed * delta
+	
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
