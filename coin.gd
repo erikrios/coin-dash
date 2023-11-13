86ab3254f9da054bfda11bffb1b8ec3d887bdb9e
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-13 10:47:55 +0700

    Adjust the coin animation

--- coin.gd
+++ coin.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 var screensize = Vector2.ZERO
 
+func _ready() -> void:
+	$Timer.start(randf_range(3, 8))
+
 func pickup():
 	$CollisionShape2D.set_deferred("disabled", true)
 	var tw := create_tween().set_parallel().set_trans(Tween.TRANS_QUAD)
@@ -9,3 +12,8 @@ func pickup():
 	tw.tween_property(self, "modulate:a", 0.0, 0.3)
 	await tw.finished
 	queue_free()
+
+
+func _on_timer_timeout() -> void:
+	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.play()
